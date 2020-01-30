c64df4e94433efa561051ab83b3654041f5383f4
wangzeping722 <werorong@gmail.com>
2020-01-30 11:01:07 +0800

    Update power-of-two.go
    n与n-1

--- Algorithms/0231.power-of-two/power-of-two.go
+++ Algorithms/0231.power-of-two/power-of-two.go
@@ -1,16 +1,12 @@
 package problem0231
 
 func isPowerOfTwo(n int) bool {
-	if n < 1 {
+	if n <= 0 {
 		return false
 	}
 
-	for n > 1 {
-		if n%2 == 1 {
-			return false
-		}
-		n /= 2
+	if n & (n-1) == 0 {
+		return true
 	}
-
-	return true
+	return false
 }
